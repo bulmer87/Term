2274f5a5ebb12d25c3d1d4a802760a5961f3413c
bulmer87 <brandonulmer87@yahoo.com>
2015-04-21 22:21:51 -0600

    Rename of Package TFIDFForTrainingSetPartThree to TFIDFForTrainingSetStepThree

---
+++ TermProject/src/TFIDFForTrainingSetStepThree/TFIDFStepThreeMapper.java
@@ -0,0 +1,17 @@
+package TFIDFForTrainingSetStepThree;
+import java.io.IOException;
+//import java.util.*;
+//import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+public class TFIDFStepThreeMapper extends Mapper<LongWritable, Text, Text, Text> 
+{
+	@Override
+	public void map(LongWritable key, Text value, Context context)throws IOException, InterruptedException 
+	{
+		String[] valArray = value.toString().split("@@@");
+		String[] keyArray = valArray[0].split("::");
+		context.write(new Text(keyArray[0]),new Text(keyArray[1] + "=" + valArray[1].trim()));
+	}
+}
